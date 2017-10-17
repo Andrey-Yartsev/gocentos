c7c179164b50f65166d568ef3fc82f1326bfc2e8
jamesarems <jamesarems@gmail.com>
2017-10-17 16:20:27 +0400

    Update 0.1 beta

--- InstallGO.sh
+++ InstallGO.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+##Install Go Lang in CentOS 7 machines.
+#Installing imp packages
+yum install wget unzip -y
+##Donwloading Go Package
+wget -O /opt/go1.9.1.linux-amd64.tar.gz https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz
+##Extract packages to a location
+tar -C /usr/local -xzf /opt/go1.9.1.linux-amd64.tar.gz
+##Export Variable path
+echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile
+##Installation completed.
+clear
+echo "GoLang has been installed succesfully"
+sleep 2s
+clear
+echo "Installation path is /usr/local/go"
+echo "For more info visit http://jamesnbr.wordpress.com"
