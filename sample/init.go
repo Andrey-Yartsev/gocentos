d978e9bb3c2aee77143755f3cf677e71ad8ed98c
jamesarems <jamesarems@gmail.com>
2017-10-18 15:07:29 +0400

    New update

---
+++ sample/init.go
@@ -0,0 +1,7 @@
+package main
+import "fmt"
+/*Testing GoLang Installatopn*/
+func main() {
+	fmt.Println("Goland has been installed successfully")
+	fmt.Println("Using http://jamesnbr.wordpress.com")
+}
